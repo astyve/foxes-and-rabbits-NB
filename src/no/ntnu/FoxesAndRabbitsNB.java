f15a51da59b98f81eeef7a23de3d4dad1e021dfb
Arne Styve <asty@ntnu.no>
2017-03-29 19:45:25 +0200

    Initial commit

---
+++ src/no/ntnu/FoxesAndRabbitsNB.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.ntnu;
+
+/**
+ *
+ * @author asty
+ */
+public class FoxesAndRabbitsNB
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        Simulator sim = new Simulator();
+        sim.runLongSimulation();
+        System.exit(0);
+    }
+    
+}
