c0c035720cce124800940a21e1c58a60483ca979
Arne Styve <asty@ntnu.no>
2017-03-30 07:33:38 +0200

    Updated Simulator etc with Animal, instead of dealing with the Rabbits and Foxes separately. Made class Animal abstract.

--- src/no/ntnu/Simulator.java
+++ src/no/ntnu/Simulator.java
@@ -26,8 +26,9 @@ public class Simulator
     private static final double RABBIT_CREATION_PROBABILITY = 0.08;    
 
     // Lists of animals in the field.
-    private List<Rabbit> rabbits;
-    private List<Fox> foxes;
+    //private List<Rabbit> rabbits;
+    //private List<Fox> foxes;
+    private List<Animal> animals;
     // The current state of the field.
     private Field field;
     // The current step of the simulation.
@@ -57,8 +58,10 @@ public class Simulator
             width = DEFAULT_WIDTH;
         }
         
-        rabbits = new ArrayList<>();
-        foxes = new ArrayList<>();
+        //rabbits = new ArrayList<>();
+        //foxes = new ArrayList<>();
+        this.animals = new ArrayList<>();
+        
         field = new Field(depth, width);
 
         // Create a view of the state of each location in the field.
@@ -101,30 +104,21 @@ public class Simulator
         step++;
 
         // Provide space for newborn rabbits.
-        List<Rabbit> newRabbits = new ArrayList<>();        
+        List<Animal> newAnimals = new ArrayList<>();        
         // Let all rabbits act.
-        for(Iterator<Rabbit> it = rabbits.iterator(); it.hasNext(); ) {
-            Rabbit rabbit = it.next();
-            rabbit.run(newRabbits);
-            if(! rabbit.isAlive()) {
+        for(Iterator<Animal> it = animals.iterator(); it.hasNext(); ) 
+        {
+            Animal animal = it.next();
+            animal.act(newAnimals);
+            if (! animal.isAlive())
+            {
                 it.remove();
             }
         }
-        
-        // Provide space for newborn foxes.
-        List<Fox> newFoxes = new ArrayList<>();        
-        // Let all foxes act.
-        for(Iterator<Fox> it = foxes.iterator(); it.hasNext(); ) {
-            Fox fox = it.next();
-            fox.hunt(newFoxes);
-            if(! fox.isAlive()) {
-                it.remove();
-            }
-        }
+
         
         // Add the newly born foxes and rabbits to the main lists.
-        rabbits.addAll(newRabbits);
-        foxes.addAll(newFoxes);
+        animals.addAll(newAnimals);
 
         view.showStatus(step, field);
     }
@@ -135,8 +129,7 @@ public class Simulator
     public void reset()
     {
         step = 0;
-        rabbits.clear();
-        foxes.clear();
+        animals.clear();
         populate();
         
         // Show the starting state in the view.
@@ -150,17 +143,20 @@ public class Simulator
     {
         Random rand = Randomizer.getRandom();
         field.clear();
-        for(int row = 0; row < field.getDepth(); row++) {
-            for(int col = 0; col < field.getWidth(); col++) {
-                if(rand.nextDouble() <= FOX_CREATION_PROBABILITY) {
+        for  (int row = 0; row < field.getDepth(); row++) 
+        {
+            for (int col = 0; col < field.getWidth(); col++) 
+            {
+                if (rand.nextDouble() <= FOX_CREATION_PROBABILITY) 
+                {
                     Location location = new Location(row, col);
                     Fox fox = new Fox(true, field, location);
-                    foxes.add(fox);
+                    animals.add(fox);
                 }
-                else if(rand.nextDouble() <= RABBIT_CREATION_PROBABILITY) {
+                else if (rand.nextDouble() <= RABBIT_CREATION_PROBABILITY) {
                     Location location = new Location(row, col);
                     Rabbit rabbit = new Rabbit(true, field, location);
-                    rabbits.add(rabbit);
+                    animals.add(rabbit);
                 }
                 // else leave the location empty.
             }
@@ -188,24 +184,13 @@ public class Simulator
      */
     private void removeDeadAnimals()
     {
-        for (Iterator<Fox> foxIt = foxes.iterator(); foxIt.hasNext();)
+        for (Iterator<Animal> animalIt = animals.iterator(); animalIt.hasNext();)
         {
-            Fox fox = foxIt.next();
-            if (!fox.isAlive())
+            Animal animal = animalIt.next();
+            if (!animal.isAlive())
             {
-                foxIt.remove();
+                animalIt.remove();
             }
-        }
-
-        for (Iterator<Rabbit> rabbitIt = rabbits.iterator(); rabbitIt.hasNext();)
-        {
-            Rabbit rabbit = rabbitIt.next();
-            if (!rabbit.isAlive())
-            {
-                rabbitIt.remove();
-            }
-        }
-        
-        
+        }        
     }
 }
